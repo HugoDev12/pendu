323d3f2dde4219ff6745661c96a72d332c1d711c
Occelli <occelli.hugo@hotmail.fr>
2021-09-26 14:47:36 +0200

    version finale du code sans le pour allez plus loin

--- js/pendu.js
+++ js/pendu.js
@@ -13,13 +13,10 @@ const WORDLIST = [
 ]
 
 let pickRandomWord = WORDLIST[Math.floor(Math.random() * WORDLIST.length)];
-let guessWord = pickRandomWord.split("").map(x => "_");//.join(" ");
-//let hiddenWord = guessWord.toString().replaceAll(","," ");
-
-//guessWord = guessWord.replaceAll(","," ");
+let guessWord = pickRandomWord.split("").map(x => "_");
 let userScore = 7;
 console.log(pickRandomWord);
-// let index = pickRandomWord.indexOf(userChoice);
+
 
 
 /*---------------------------Functions-----------------*/
@@ -41,9 +38,6 @@ let index = pickRandomWord.indexOf(userChoice);
             };
         };
     } else userScore --;
-
-    // guessWord = guessWord.toString();
-    // guessWord = guessWord.replaceAll(","," ");
 };
 
 
@@ -55,206 +49,6 @@ while ((userScore != 0) && (guessWord.includes("_"))){
     main();
 }
 
-userScore === 0 ? alert ("Vous avez perdu") : alert ("Vous avez gagné");
+userScore === 0 ? alert (`Vous avez perdu, le mot était ${pickRandomWord}.`) : alert (`Vous avez gagné, vous avez trouvé le mot ${pickRandomWord}.`);
 
-console.log(guessWord, userScore);
 
-
-
-
-
-//let userChoice = prompt (`Il vous reste ${userScore} essais\n\n${guessWord}\n\n\n Choisissez une lettre.`);
-
-    //= prompt(guessWord + "\n\n\n Choisissez une lettre.");
-
-    //= prompt (`${userScore}\n\n${guessWord}\n\n\n Choisissez une lettre.`);
-
-//let index = pickRandomWord.indexOf(userChoice);
-
-//guessWord = guessWord.replace(guessWord.charAt(index),userChoice);
-
-//guessWord[index] = userChoice;
-
-
-
-
-
-
-
-
-
-//function main(userScore){
-
-//let userChoice = prompt (`Il vous reste ${userScore} essais\n\n${guessWord}\n\n\n Choisissez une lettre.`);
-// let userChoice = "";
-// // let index = pickRandomWord.indexOf(userChoice);
-
-
-// // for (let i=0; i < guessWord.length; i++){
-
-// //     if (pickRandomWord[i] === userChoice)
-// //     {
-// //         guessWord[i] = userChoice;
-// //     };
-// // };
-
-// function main (userChoice, userScore){
-
-//        userChoice = prompt (`Il vous reste ${userScore} essais\n\n${guessWord}\n\n\n Choisissez une lettre.`);
-//    let index = pickRandomWord.indexOf(userChoice);
-
-
-//     while ((/^[a-zA-Z]+$/.test(userChoice)) || (pickRandomWord[index]))
-//     {
-//         for (let i=0; i < guessWord.length; i++){
-
-//             if (pickRandomWord[i] === userChoice)
-//             {
-//                 guessWord[i] = userChoice;
-//             } else userScore --;
-//         };
-        
-//     }
-
-//     return userChoice, userScore;
-// };
-
-// main(userChoice, userScore);
-
-// console.log(guessWord, userScore);
-
-
-//     if (/^[a-zA-Z]+$/.test(userChoice)){
-
-//         for (let i=0; i < guessWord.length; i++){
-
-//             if (pickRandomWord[i] === userChoice)
-//             {
-//                 guessWord[i] = userChoice;
-//             };
-//         };
-//     } else {
-//       console.log("test");
-//     }
-// return userScore;
-// }
-
-
-// function main(userScore){
-
-// let userChoice = prompt (`Il vous reste ${userScore} essais\n\n${guessWord}\n\n\n Choisissez une lettre.`);
-// let index = pickRandomWord.indexOf(userChoice);
-
-// if (pickRandomWord[index]){
-//     for (let i=0; i < guessWord.length; i++){
-
-//         if (pickRandomWord[i] === userChoice)
-//         {
-//             guessWord[i] = userChoice;
-//         };
-//     };
-// } else userScore--;
-
-// return userScore;
-// };
-
-
-// if (/^[a-zA-Z]+$/.test(userChoice)){
-
-//     while (guessWord.includes("_") || userScore === 0){
-//     main(userScore);
-//     }
-// } else main(userScore);
-
-
-// for (let i=0; i < guessWord.length; i++){
-
-//     if (pickRandomWord[i] === userChoice)
-//     {
-//         guessWord[i] = userChoice;
-//     };
-// };
-
-
-//function scrollGuessWord 
-// while (index){
-//     guessWord[index] = userChoice;
-// }
-
-//console.log(guessWord.charAt((index*2)-1));
-
-// alert(pickRandomWord);
-// alert(guessWord);
-
-//let guessWord = wordLetters.map(x => "_")//.join(" ");
-
-//console.log(typeof guessWord);
-
-// let userChoice = prompt(guessWord + "\n\n\n Choisissez une lettre.");
-
-// let idx = pickRandomWord.indexOf(userChoice);
-
-// console.log(guessWord.charAt((idx*2)-1));
-
-// //guessWord = guessWord.replace((guessWord.charAt(idx*2)),userChoice);
-// guessWord = guessWord.replace(guessWord.charAt(19), userChoice);
-
-// alert(guessWord);
-
-//let element = userChoice;
-
-//let indices = [];
-//console.log(guessWord.charAt(10));// = userChoice;
-
-// let idx = pickRandomWord.indexOf(userChoice);
-// while (idx != -1){
-//     guessWord.charAt(idx*2) = userChoice;
-//     indices.push(idx);
-//     idx = pickRandomWord.indexOf(userChoice, idx +1);
-// }
-
-//alert(guessWord);
-
-//console.log(guessWord);
-
-//console.log(mapWord.fill())
-
-
-//let fillLetter = wordLetters.indexOf(userChoice, userChoice);
-//wordLetters.indexOf(userChoice);
-
-
-
-
-
-// for (let hideLetter of wordLetters){
-//     hideLetter = "_";
-    // console.log(hideLetter);
-// }
-
-
-
-// const correctLetter = (currentLetter) => currentLetter != "l";
-
-// console.log(wordLetters.every(correctLetter));
-
-//wordLetters.includes("l") ? console.log("true") : console.log("false");
-
-//console.log(hideLetter);
-
-
-
-
-// function wordGuess(wordLetters){
-//     let attempts = 7;
-//     let userLetter = prompt("choisissez une lettre");
-// }
-
-
-
-
-
-
-
-
-//alert("Bienvenue ! vous allez jouer au pendu !");
